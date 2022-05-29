087c8a1238e854e56d5c40cf9725fc29107cf4c9
Leopold Johannes Meinel <leo@meinel.dev>
2022-05-29 15:23:07 +0200

    Initial commit
    Signed-off-by: Leopold Johannes Meinel <leo@meinel.dev>

---
+++ src/lib.rs
@@ -0,0 +1,43 @@
+/*
+ * app_cli is a commandline application.
+ * Copyright © 2022 Leopold Meinel & contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see https://github.com/TamrielNetwork/app_cli/blob/main/LICENSE
+ */
+
+use std::env;
+
+pub mod grep;
+
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+    pub case_sensitive: bool,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments!");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        let case_sensitive = env::var("CASE_SENSITIVE").is_ok();
+        Ok(Config {
+            query,
+            filename,
+            case_sensitive,
+        })
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,34 @@
+/*
+ * app_cli is a commandline application.
+ * Copyright © 2022 Leopold Meinel & contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see https://github.com/TamrielNetwork/app_cli/blob/main/LICENSE
+ */
+
+use std::{env, process};
+
+use app_cli::{grep, Config};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        // Print to standard error stream
+        eprintln!("ERROR: Problem parsing arguments! - {}", err);
+        process::exit(1);
+    });
+    if let Err(err) = grep::grep(config) {
+        eprintln!("ERROR: {}", err);
+        process::exit(1);
+    }
+}
